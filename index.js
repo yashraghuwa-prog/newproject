5d59170e19425a4238940daa50a3c23b84efc143
Yash Raghuwanshi <yashraghuwa@gmail.com>
2026-09-13 11:10:56 +0530

    commit

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const path=require("path");
 const express =require("express");
+const mongoose=require('mongoose');
+
+const userRoute =require('./routes/user')
 const app=express();
 const PORT=8000;
-app.set('viewengine','ejs');
+
+mongoose.connect('mongodb://localhost:27017/blogify').then(e=>console.log('MongoDB connected'));
+
+app.set('view engine','ejs');
 app.set('views',path.resolve('./views'));
+
+app.use(express.urlencoded({extended:false}));
+
+app.get('/',(req,res)=>{
+    res.render("home");
+})
+app.use('/user',userRoute);
+
 app.listen(PORT,()=>console.log(`server started at porst:${PORT}`));
